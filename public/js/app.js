059b836cbf94c84b71c9ce5bb920afeb701a9b34
IKEA <jellynina@gmail.com>
2016-01-12 20:06:04 +0800

    Add basic form and firebase

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,63 @@
-var app = angular.module('powerup', ['ngRoute', 'firebase']);
+var app = angular.module('power-up', ['ngRoute', 'firebase']);
 
-//The Firebase URL set as a Constant
-app.constant('fbURL', 'https://power-up.firebaseio.com/');
+app.constant('fbUrl', 'https://power-up.firebaseio.com/');
+app.constant('fbUrlStores', 'https://power-up.firebaseio.com/stores/')
 
-//Configuring the route providers to redirect to the right location
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            controller: 'MainController',
-            templateUrl: 'main.html'
-        })
-        .otherwise({
-            redirectTo: '/'
-        })
+app.factory('Stores', function ($firebase, fbUrl, fbUrlStores) {
+    return $firebase(new Firebase(fbUrlStores)).$asArray();
 });
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'MainController',
+      templateUrl: '../main.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+});
+
+
+app.controller('MainController', function ($scope, $firebase, Stores) {
+    $scope.savePower = function(store) {
+      if (store.city && store.address && store.time && store.title) {
+
+        Stores.$add({
+          titile: store.title,
+          city: store.city,
+          address: store.address,
+          time: store.time
+        })
+        store.title = "";
+        store.city = "";
+        store.address = "";
+        store.time = "";
+
+      }else {
+        alert("Plz fill all");
+      }
+    }
+});
+// app.controller('MainController', function ($scope, $firebase, Stores) {
+//
+//   $scope.stores = Stores;
+//
+//   $scope.savePower = function(store) {
+//     if (store.city && store.address && store.time) {
+//
+//       Stores.$add({
+//         city: store.city,
+//         address: store.address,
+//         time: store.time
+//       })
+//
+//       store.city = "";
+//       store.address = "";
+//       store.time = "";
+//
+//     }else {
+//       alert("Plz fill all");
+//     }
+//   }
+//
+// });
