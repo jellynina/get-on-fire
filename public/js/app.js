f839595803c04bec3e968e13565513c6fe34c2b9
IKEA <jellynina@gmail.com>
2016-02-05 03:08:29 +0800

    add auto complete place

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('power-up', ['ngRoute', 'firebase']);
+var app = angular.module('power-up', ['ngRoute', 'firebase', 'ngAutocomplete']);
 
 app.constant('fbUrl', 'https://power-up.firebaseio.com/');
 app.constant('fbUrlStores', 'https://power-up.firebaseio.com/stores/')
@@ -42,4 +42,54 @@ app.controller('MainController', function ($scope, $firebase, Stores) {
         alert("Plz fill all");
       }
     }
+
+    $scope.result = ''
+    $scope.options = {};
+
+    $scope.form = {
+      type: 'geocode',
+      bounds: {SWLat: 49, SWLng: -97, NELat: 50, NELng: -96},
+      country: 'ca',
+      typesEnabled: false,
+      boundsEnabled: false,
+      componentEnabled: false,
+      watchEnter: true
+    }
+
+    //watch form for changes
+    $scope.watchForm = function () {
+      return $scope.form
+    };
+    $scope.$watch($scope.watchForm, function () {
+      $scope.checkForm();
+    }, true);
+
+
+    //set options from form selections
+    $scope.checkForm = function() {
+
+      $scope.options = {
+
+      };
+
+
+      $scope.options.watchEnter = $scope.form.watchEnter
+
+      if ($scope.form.typesEnabled) {
+        $scope.options.types = $scope.form.type
+      }
+      if ($scope.form.boundsEnabled) {
+
+        var SW = new google.maps.LatLng($scope.form.bounds.SWLat, $scope.form.bounds.SWLng)
+        var NE = new google.maps.LatLng($scope.form.bounds.NELat, $scope.form.bounds.NELng)
+        var bounds = new google.maps.LatLngBounds(SW, NE);
+        $scope.options.bounds = bounds
+
+      }
+      if ($scope.form.componentEnabled) {
+        $scope.options.country = $scope.form.country
+      }
+    };
+
+
 });
